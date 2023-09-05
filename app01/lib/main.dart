19c73b727e29cca3b76d3636c7998c7ba43b4767
LimberthStyle <202001421b@utea.edu.pe>
2023-09-05 16:42:03 -0500

    Reto AVANCE 1

--- app01/lib/main.dart
+++ app01/lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xffccd5ae),
+        backgroundColor: Color.fromARGB(255, 251, 251, 251),
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                width: 300.0,
-                height: 300.0,
-                color: Colors.red,
+                width: 380,
+                height: 250,
+                color: Color.fromARGB(255, 245, 245, 245),
                 child: Image.network(
-                  "https://images.pexels.com/photos/2007647/pexels-photo-2007647.jpeg",
+                  "https://holidaystoswitzerland.com/wp-content/uploads/2020/03/Lake-Oeschinensee-Switzerland-1.jpg",
                   fit: BoxFit.fill,
                 ),
-              )
+              ),
+              Container(
+                width: 300,
+                height: 300,
+                color: Color.fromARGB(255, 254, 254, 254),
+                child: Text(
+                  "Oeschinen Lake Campground",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
             ],
           ),
         ),
