74f09ca87be0b1e6461a46e34dd47398d7eb05f2
LimberthStyle <202001421b@utea.edu.pe>
2023-09-15 07:42:00 -0500

    CONTROL DE COLOR

--- flutter_app_state/lib/Test2_Page.dart
+++ flutter_app_state/lib/Test2_Page.dart
@@ -10,6 +10,9 @@ class Test2Page extends StatefulWidget {
 class _Test2PageState extends State<Test2Page> {
   bool isTitleBold = true;
   bool isDescriptionJustify = false;
+  double valueRedSlider = 0;
+  double valueGreenSlider = 0;
+  double valueBlueSlider = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +36,10 @@ class _Test2PageState extends State<Test2Page> {
               "Phasellus lacinia accumsan odio, non porta lorem mattis vel. Mauris volutpat viverra interdum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus hendrerit, felis in bibendum commodo, tellus libero sollicitudin mi, ac suscipit massa dui eget nisl. Vivamus maximus lectus vel massa ornare vulputate. ",
               textAlign:
                   isDescriptionJustify ? TextAlign.justify : TextAlign.start,
+              style: TextStyle(
+                color: Color.fromRGBO(valueRedSlider.toInt(),
+                    valueGreenSlider.toInt(), valueBlueSlider.toInt(), 1),
+              ),
             ),
             /* Checkbox(
                 value: isTitleBold,
@@ -59,6 +66,36 @@ class _Test2PageState extends State<Test2Page> {
                   print(value);
                   setState(() {});
                 }),
+            Slider(
+              min: 0,
+              max: 255,
+              value: valueRedSlider,
+              onChanged: (value) {
+                print(value);
+                valueRedSlider = value;
+                setState(() {});
+              },
+            ),
+            Slider(
+              min: 0,
+              max: 255,
+              value: valueGreenSlider,
+              onChanged: (value) {
+                print(value);
+                valueGreenSlider = value;
+                setState(() {});
+              },
+            ),
+            Slider(
+              min: 0,
+              max: 255,
+              value: valueBlueSlider,
+              onChanged: (value) {
+                print(value);
+                valueBlueSlider = value;
+                setState(() {});
+              },
+            )
           ],
         ),
       ),
