3f6430e3854c072fedbad9d11861b927f8ad00a5
LimberthStyle <202001421b@utea.edu.pe>
2023-10-04 07:25:31 -0500

    Card_and_Avatar

--- app_components/lib/pages/home_page.dart
+++ app_components/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'alert_page.dart';
+import 'avatar_page.dart';
+import 'card_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,28 +11,116 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                //color: Colors.redAccent,
-                image: DecorationImage(
-                  image: NetworkImage(
-                      "https://images.pexels.com/photos/18103058/pexels-photo-18103058/free-photo-of-resfriado-nieve-invierno-perros.jpeg"),
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    offset: Offset(5, 5),
-                    blurRadius: 12.0,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20.0,
               ),
-            ),
-          ],
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      offset: Offset(8, 8),
+                      blurRadius: 10,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(20.0),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle-768x770.jpg"),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Flutter Components",
+                style: GoogleFonts.poppins(
+                  fontSize: 22.0,
+                  letterSpacing: 1,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 200.0,
+                child: Divider(),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ItemComponentWidget(
+                title: "Avatar",
+                toPage: AvatarPage(),
+              ),
+              ItemComponentWidget(
+                title: "Alert",
+                toPage: AlertPage(),
+              ),
+              ItemComponentWidget(
+                title: "Card",
+                toPage: CardPage(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemComponentWidget extends StatelessWidget {
+  String title;
+  Widget toPage;
+
+  ItemComponentWidget({required this.title, required this.toPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 15.0,
+        vertical: 15.0,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(
+          10.0,
+        ),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 12,
+              offset: Offset(5, 5)),
+        ],
+      ),
+      child: ListTile(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => toPage),
+          );
+        },
+        leading: Icon(
+          Icons.check_circle_outline,
+          color: Colors.black,
+        ),
+        title: Text(
+          title,
+          style: GoogleFonts.poppins(),
+        ),
+        subtitle: Text(
+          "ir al detalle de $title",
+          style: GoogleFonts.poppins(),
+        ),
+        trailing: Icon(
+          Icons.chevron_right,
         ),
       ),
     );
